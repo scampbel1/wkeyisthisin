fd58d0b9b2622510f4002a86c910797071b2ef62
Sean Campbell <sjpcamp@gmail.com>
2018-12-01 23:50:30 +0000

    Scales REST endpoint added. Parameterised controller methods not working as expected. Reviewing the manual!

---
+++ KeyifyWebApplication/Controllers/ScalesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Web.Http;
+using KeyifyScaleFinderClassLibrary.MusicTheory;
+
+namespace KeyifyWebApplication.Controllers
+{
+    public class ScalesController : ApiController
+    {
+        public IHttpActionResult Get(string scale)
+        {
+            try
+            {
+                var scaleEntry = ScaleDictionary.GenerateEntryFromString(scale);
+                return Ok(scaleEntry.Scale.Notes);
+            }
+            catch (Exception e)
+            {
+                return InternalServerError(e);
+            }
+        }
+    }
+}
