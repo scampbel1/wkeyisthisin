05e6a2722f0924ea5bb57af6fb05f34369067c74
Sean Campbell <sjpcamp@gmail.com>
2019-04-20 11:11:45 +0100

    JsonResult Poc added for reference

---
+++ Poc/C# Testing Projects/JsonResultTypeinMvc/WebApplication1/Models/UserModel.cs
@@ -0,0 +1,9 @@
+﻿namespace WebApplication1.Models
+{
+    public class UserModel
+    {
+        public int UserId { get; set; }
+        public string UserName { get; set; }
+        public string Company { get; set; }
+    }
+}
