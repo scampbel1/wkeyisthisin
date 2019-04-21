f7461fac96c2e0590b014d00f8615a55bb9160bc
Sean Campbell <sjpcamp@gmail.com>
2019-04-21 10:25:20 +0100

    Value passing over to parameter... working!

--- KeyifyWebClient.Core/Controllers/HomeController.cs
+++ KeyifyWebClient.Core/Controllers/HomeController.cs
@@ -21,9 +21,10 @@ namespace KeyifyWebClient.Core.Controllers
         }
 
         [HttpPost]
-        public JsonResult UpdateFretboardModel(string data)
+        public JsonResult UpdateFretboardModel([FromBody] string note)
         {
             //Update: 20/04/2019 - Poc created, demonstrating Json functionality
+            //Update: 21/04/2019 - Value being passed - JSON format was incorrect
             //TODO: Complete method
             return null;
         }
