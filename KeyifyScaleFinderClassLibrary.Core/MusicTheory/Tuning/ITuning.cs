125f0533485eb4fb02f2c8ccb3b55c98874dd717
Sean Campbell <sjpcamp@gmail.com>
2019-09-03 22:22:27 +0100

    Fret count updated, git cache cleared following this --> https://stackoverflow.com/questions/11123248/gitignore-and-visual-studio-project-ignore-bin-debug-directory-but-not-bin-rel/11123628

--- KeyifyScaleFinderClassLibrary.Core/MusicTheory/Tuning/ITuning.cs
+++
@@ -1,9 +0,0 @@
-﻿using KeyifyScaleFinderClassLibrary.Core.MusicTheory.Enums;
-
-namespace KeyifyScaleFinderClassLibrary.Core.MusicTheory.Tuning
-{
-    public interface ITuning
-    {
-        Note[] ReturnNotes();
-    }
-}
--- Poc/C# Testing Projects/JsonResultTypeinMvc/WebApplication1/Models/UserModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace WebApplication1.Models
-{
-    public class UserModel
-    {
-        public int UserId { get; set; }
-        public string UserName { get; set; }
-        public string Company { get; set; }
-    }
-}
