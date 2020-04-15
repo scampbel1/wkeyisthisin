5cb1f5061cf089e5ff7a896ed400608d78f86d38
Sean <Sean@DESKTOP-IGDDAAA>
2020-04-15 11:30:28 +0100

    New project (Working in Docker) added to solution

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Keyify.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
