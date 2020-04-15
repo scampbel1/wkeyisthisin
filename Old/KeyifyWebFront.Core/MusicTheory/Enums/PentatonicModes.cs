61bdcf182f5b4a132e7cec13fc00165a60f7d821
Sean <Sean@DESKTOP-IGDDAAA>
2020-04-15 12:17:04 +0100

    Removed old files, slowly going to move them back in and focus on keeping working solution intact

--- Old/KeyifyWebFront.Core/MusicTheory/Enums/PentatonicModes.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace KeyifyClassLibrary.Core.MusicTheory.Enums
-{
-    public enum PentatonicModes
-    {
-        Minor,
-        Major
-    }
-}
--- Old/KeyifyWebFront.Core/MusicTheory/Tuning/ITuning.cs
+++
@@ -1,9 +0,0 @@
-﻿using KeyifyClassLibrary.Core.MusicTheory.Enums;
-
-namespace KeyifyClassLibrary.Core.MusicTheory.Tuning
-{
-    public interface ITuning
-    {
-        Note[] ReturnNotes();
-    }
-}
