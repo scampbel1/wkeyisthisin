86b1781ad649e21f766c2085357d4d3a351a5ae9
Sean <Sean@DESKTOP-IGDDAAA>
2020-05-03 15:33:56 +0100

    Guitar controller created

---
+++ Controllers/GuitarController.cs
@@ -0,0 +1,68 @@
+﻿using KeyifyClassLibrary.Core.MusicTheory;
+using KeyifyClassLibrary.Core.MusicTheory.Enums;
+using KeyifyClassLibrary.Core.MusicTheory.Helper;
+using KeyifyWebClient.Core.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Keyify.Controllers
+{
+    public class GuitarController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            var model = new FretboardWebModel();
+            return View(model);
+        }
+
+        [HttpPost]
+        public ActionResult UpdateFretboardModel(string[] notes, string scale)
+        {
+            FretboardWebModel model = new FretboardWebModel();
+
+            if (notes != null && notes.Length > 0)
+            {
+                List<Note> realNotes = ElementStringConverter.ConvertStringArrayIntoNotes(notes);
+
+                model.ApplySelectedNotesToFretboard(notes);
+                model.Scales = ScaleMatcher.GetMatchedScales(realNotes);
+                model.SelectedNotes = new List<string>(notes);
+            }
+
+            if (!string.IsNullOrEmpty(scale))
+            {
+                model.SelectedScale = ScaleDictionary.GenerateEntryFromString(scale);
+
+                var selected = new ScaleMatch(model.SelectedScale.ScaleName, model.SelectedScale.Scale.Notes);
+                selected.Selected = true;
+
+                if (!model.Scales.Any(a => a.ScaleName == selected.ScaleName))
+                    model.Scales.Add(selected);
+                else
+                {
+                    var update = model.Scales.Single(a => a.ScaleName == selected.ScaleName);
+                    model.Scales.Remove(update);
+                    model.Scales.Add(selected);
+                }
+
+                model.ApplySelectedScaleNotesToFretboard(model.SelectedScale.Scale.NotesSet);
+            }
+            else
+                model.SelectedScale = null;
+
+            model.Scales = model.Scales.OrderBy(a => a.ScaleName).ToList();
+
+            return PartialView("Guitar", model);
+        }
+
+        [HttpPost]
+        public ActionResult ResetModel()
+        {
+            var model = new FretboardWebModel();
+
+            return PartialView("Guitar", model);
+        }
+    }
+}
