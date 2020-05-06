a02c8fac01b0e5416bb07da62ff37053cb55c3cb
Sean <Sean@DESKTOP-IGDDAAA>
2020-05-06 12:32:37 +0100

    Added a memory cache on startup of application (this push should fail)

--- Startup.cs
+++ Startup.cs
@@ -24,6 +24,7 @@ namespace Keyify
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
