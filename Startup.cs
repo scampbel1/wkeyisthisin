12f29676ec35c9a99964000b799b132fddea9195
Sean <Sean@DESKTOP-IGDDAAA>
2020-05-06 12:41:18 +0100

    Removed adding of memorycache... this shouldn't be pushed (hopefully)

--- Startup.cs
+++ Startup.cs
@@ -24,7 +24,6 @@ namespace Keyify
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
-            services.AddMemoryCache();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
