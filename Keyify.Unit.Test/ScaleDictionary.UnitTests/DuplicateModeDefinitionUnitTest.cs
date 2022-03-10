736991055566be14619b581202e415e58b5e263f
Sean <22592487+scampbel1@users.noreply.github.com>
2022-03-10 20:10:56 +0000

    Temporarily skipping TDD chord unit tests

--- Keyify.Unit.Test/ScaleDictionary.UnitTests/DuplicateModeDefinitionUnitTest.cs
+++ Keyify.Unit.Test/ScaleDictionary.UnitTests/DuplicateModeDefinitionUnitTest.cs
@@ -10,13 +10,22 @@ namespace Keyify.Unit.Test.ScaleDictionary.UnitTests
     {
         private static List<ModeDefinition> _scaleEntries = new ModeDefinitionService().GetModeDefinitions();
 
-        [Fact (Skip = "Fix this later, there were 5 duplicated scale step groups at time of writing.")]
-        public void NoDuplicateModeDefinitions()
+        [Fact(Skip = "")]
+        public void NoDuplicateModeDefinitionsByScaleDegrees()
         {
             var scaleSteps = _scaleEntries.Select(s => string.Join(",", s.ScaleDegrees));
             var scaleStepDuplicates = scaleSteps.GroupBy(g => g).Where(s => s.Count() > 1);
 
-            Assert.True(scaleStepDuplicates.Count() == 0);
+            Assert.False(scaleStepDuplicates.Any());
+        }
+
+        [Fact(Skip = "")]
+        public void NoDuplicateModeDefinitionsByScaleSteps()
+        {
+            var scaleSteps = _scaleEntries.Select(s => string.Join(",", s.ScaleSteps));
+            var scaleStepDuplicates = scaleSteps.GroupBy(g => g).Where(s => s.Count() > 1);
+
+            Assert.False(scaleStepDuplicates.Any());
         }
     }
 }
