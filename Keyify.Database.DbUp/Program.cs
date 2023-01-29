684a65ad8ebbe9e298d72d4ca2081f90354a3fa4
Sean <22592487+scampbel1@users.noreply.github.com>
2023-01-29 22:55:08 +0000

    Copied in barebones DbUp code

--- Keyify.Database.DbUp/Program.cs
+++ Keyify.Database.DbUp/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using DbUp;
+using System.Reflection;
+
+internal class Program
+{
+    public static int Main(string[] args)
+    {
+        var connectionString =
+            args.FirstOrDefault()
+            ?? "Server=(local)\\SqlExpress; Database=MyApp; Trusted_connection=true";
+
+        var upgrader =
+            DeployChanges.To
+                .SqlDatabase(connectionString)
+                .WithScriptsEmbeddedInAssembly(Assembly.GetExecutingAssembly())
+                .LogToConsole()
+                .Build();
+
+        var result = upgrader.PerformUpgrade();
+
+        if (!result.Successful)
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(result.Error);
+            Console.ResetColor();
+#if DEBUG
+            Console.ReadLine();
+#endif
+            return -1;
+        }
+
+        Console.ForegroundColor = ConsoleColor.Green;
+        Console.WriteLine("Success!");
+        Console.ResetColor();
+        return 0;
+    }
+}
