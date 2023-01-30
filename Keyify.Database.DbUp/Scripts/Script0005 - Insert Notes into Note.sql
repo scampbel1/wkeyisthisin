50ee9dddc1991a6c757afaec2d4f65347e84833a
Sean <22592487+scampbel1@users.noreply.github.com>
2023-01-30 18:00:34 +0000

    Inserting Note values into Core.Note

---
+++ Keyify.Database.DbUp/Scripts/Script0005 - Insert Notes into Note.sql
@@ -0,0 +1,3 @@
+INSERT INTO [Core].[Note] ([Flat], [Sharp])
+VALUES ('A', 'A'), ('Bb', 'A#'), ('B', 'B'), ('C', 'C'), ('Db', 'C#'), ('D', 'D'), ('Eb', 'D#'), ('E', 'E'), ('F', 'F'), ('Gb', 'F#'), ('G', 'G'), ('Ab', 'G#')
+GO
