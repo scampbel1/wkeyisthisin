2f3297ff016bfcbe8b7b82ed80eb2862b6116077
Sean <22592487+scampbel1@users.noreply.github.com>
2023-01-30 18:12:35 +0000

    Chord types inserted into database. Mode enum files renamed to match type name

---
+++ Keyify.Database.DbUp/Scripts/Script0006 - Insert ChordTypes into ChordType.sql
@@ -0,0 +1,4 @@
+INSERT INTO [Core].[ChordType] ([Name])
+VALUES ('Major'), ('Minor'), ('Diminished'), ('MajorSeventh'), ('MinorSeventh'), ('DominantSeventh'), ('SuspendedSecond'), ('SuspendedFourth'), ('Augmented'), ('MajorNinth'), ('MinorNinth'), ('DominantNinth'), ('MajorEleventh'), ('MinorEleventh'), ('DominantEleventh'), ('MajorThirteenth'), ('MinorThirteenth'), ('DominantThirteenth')
+
+GO
