4547b51d1b8fd027c7db776fe686c06668aee96c
Sean <22592487+scampbel1@users.noreply.github.com>
2023-01-31 22:49:39 +0000

    Sample ThrowawayDb Integration Test created using sample code from Github Repo

---
+++ Keyify.Database.Integration.Test/ChordDatabaseTest.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Data.SqlClient;
+using ThrowawayDb;
+using Xunit;
+
+namespace Keyify.Database.Integration.Test
+{
+    public class ChordDatabaseTest
+    {
+        //https://khalidabuhakmeh.com/dotnet-database-integration-tests
+        //https://github.com/Zaid-Ajaj/ThrowawayDb
+
+        [Fact]
+        public void Can_Select_Chord_Entry_From_Database()
+        {
+            using (var database = ThrowawayDatabase.FromLocalInstance("."))
+            {
+                Console.WriteLine($"Created database {database.Name}");
+
+                // - Apply database migrations here if necessary
+                // - Seed the database with data
+                // - Execute your code against this database
+                using var connection = new SqlConnection(database.ConnectionString);
+                connection.Open();
+                using var cmd = new SqlCommand("SELECT 1", connection);
+                var result = Convert.ToInt32(cmd.ExecuteScalar());
+                Console.WriteLine(result);
+            }
+        }
+    }
+}
