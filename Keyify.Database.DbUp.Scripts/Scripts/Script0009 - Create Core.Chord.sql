09ac15820662a4ce5448ee9a9cfd03822c5b7a25
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-03 08:09:22 +0000

    Fixed Tuning and Chord tables

--- Keyify.Database.DbUp.Scripts/Scripts/Script0009 - Create Core.Chord.sql
+++ Keyify.Database.DbUp.Scripts/Scripts/Script0009 - Create Core.Chord.sql
@@ -3,10 +3,10 @@ CREATE TABLE Core.Chord (
 	,[ChordTypeId] INT NOT NULL
 	,[RootNoteId] INT NOT NULL
 	,[TuningId] INT NOT NULL
-	,[Tabs] BINARY NOT NULL
+	,[Tabs] VARBINARY(MAX) NOT NULL
 	,[Created] DATETIME NOT NULL
-	,[LastModified] DATETIME NOT NULL
-	,[Deleted] BIT NOT NULL
+	,[LastModified] DATETIME NULL
+	,[Deleted] BIT NOT NULL DEFAULT 0
 	,CONSTRAINT PK_Chord_Id PRIMARY KEY (Id)
 	,CONSTRAINT FK_ChordTypeId FOREIGN KEY (ChordTypeId) REFERENCES Core.ChordType(Id)
 	,CONSTRAINT FK_RootNoteId FOREIGN KEY (RootNoteId) REFERENCES Core.Note(Id)
