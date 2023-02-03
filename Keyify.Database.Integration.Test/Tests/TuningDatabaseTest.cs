a44286782e798677437f05a91daab9ba146e52b6
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-03 23:31:34 +0000

    Created tuning record integration test

---
+++ Keyify.Database.Integration.Test/Tests/TuningDatabaseTest.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using Keyify.Database.Integration.Test.Enums;
+using Keyify.Database.Integration.Test.Helper;
+using Keyify.Database.Integration.Test.ThrowawayDatabases;
+using Microsoft.Data.SqlClient;
+using System.Text.Json;
+using Xunit;
+
+namespace Keyify.Database.Integration.Test.Tests
+{
+    public class TuningDatabaseTest
+    {
+        [Fact]
+        public async Task CreateTuning_RecordCreatedInDatabase_ReturnsSingleRecord_ReturnsCorrectNotesIntArray()
+        {
+            using (var throwawayDbInstance = await ThrowawayDatabaseSetup.CreateThrowawayDbInstanceAsync())
+            {
+                using var sqlCconnection = new SqlConnection(throwawayDbInstance.ConnectionString);
+                sqlCconnection.Open();
+
+                //Arrange
+                await sqlCconnection.ExecuteAsync(DatabaseTestHelper.CreateInsertTuningSqlScript(), DatabaseTestHelper.CreateInsertStandardTuningSqlScriptParameters());
+
+                //Act
+                var sqlQuery = "SELECT [Notes] FROM [Core].[Tuning]";
+                var tuning = await sqlCconnection.QuerySingleAsync(sqlQuery);
+
+                using var memoryStream = new MemoryStream(tuning.Notes);
+                var notesResult = await JsonSerializer.DeserializeAsync<int[]>(memoryStream);
+
+                await sqlCconnection.CloseAsync();
+
+                //Assert
+                Assert.Single(tuning);
+                Assert.Equal(GuitarTuningConstant.StandardTuning, notesResult);
+            }
+        }
+    }
+}
