c94e191f412e57aad0e69c0c52ec911b466e2514
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-04 21:08:25 +0000

    Created QuickLinkParameters model

---
+++ Keyify.Web/Models/QuickLink/QuickLinkParameters.cs
@@ -0,0 +1,17 @@
+﻿using Keyify.Web.Enums;
+using Keyify.Web.Enums.Tuning;
+using KeyifyClassLibrary.Enums;
+
+namespace Keyify.Web.Models.QuickLink
+{
+    public class QuickLinkParameters
+    {
+        public InstrumentType InstrumentType { get; set; }
+
+        public GuitarTuning Tuning { get; set; }
+
+        public Note[] SelectedNotes { get; set; }
+
+        public string SelectedScale { get; set; }
+    }
+}
