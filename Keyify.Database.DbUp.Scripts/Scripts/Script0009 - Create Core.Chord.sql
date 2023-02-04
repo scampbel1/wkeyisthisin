a5180ea715ebda7f156bdaa5d7ee0ab49bbecf65
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-04 10:25:52 +0000

    Simplified primary keys on table creation scripts

--- Keyify.Database.DbUp.Scripts/Scripts/Script0009 - Create Core.Chord.sql
+++ Keyify.Database.DbUp.Scripts/Scripts/Script0009 - Create Core.Chord.sql
@@ -1,19 +1,18 @@
 CREATE TABLE Core.Chord (
-	[Id] INT IDENTITY(0, 1) NOT NULL
-	,[ChordTypeId] INT NOT NULL
-	,[RootNoteId] INT NOT NULL
-	,[TuningId] INT NOT NULL
-	,[Name] NVARCHAR(100) NOT NULL
-	,[Description] INT NULL
-	,[Tabs] VARBINARY(MAX) NOT NULL
-	,[Created] DATETIME NOT NULL DEFAULT GETUTCDATE()
-	,[LastModified] DATETIME NULL
-	,[Deleted] BIT NOT NULL DEFAULT 0
-	,CONSTRAINT PK_Chord_Id PRIMARY KEY (Id ASC)
-	,CONSTRAINT FK_ChordTypeId FOREIGN KEY (ChordTypeId) REFERENCES Core.ChordType(Id)
-	,CONSTRAINT FK_RootNoteId FOREIGN KEY (RootNoteId) REFERENCES Core.Note(Id)
-	,CONSTRAINT FK_TuningId FOREIGN KEY (TuningId) REFERENCES Core.Tuning(Id)
-	,CONSTRAINT [UQ_Instrument_Chord] UNIQUE NONCLUSTERED
+	[Id] INT IDENTITY(0, 1) NOT NULL PRIMARY KEY,
+	[ChordTypeId] INT NOT NULL,
+	[RootNoteId] INT NOT NULL,
+	[TuningId] INT NOT NULL,
+	[Name] NVARCHAR(100) NOT NULL,
+	[Description] INT NULL,
+	[Tabs] VARBINARY(MAX) NOT NULL,
+	[Created] DATETIME NOT NULL DEFAULT GETUTCDATE(),
+	[LastModified] DATETIME NULL,
+	[Deleted] BIT NOT NULL DEFAULT 0	,
+	CONSTRAINT FK_ChordTypeId FOREIGN KEY (ChordTypeId) REFERENCES Core.ChordType(Id),
+	CONSTRAINT FK_RootNoteId FOREIGN KEY (RootNoteId) REFERENCES Core.Note(Id),
+	CONSTRAINT FK_TuningId FOREIGN KEY (TuningId) REFERENCES Core.Tuning(Id),
+	CONSTRAINT [UQ_Instrument_Chord] UNIQUE NONCLUSTERED
     (
         [TuningId], [RootNoteId], [ChordTypeId], [Deleted]
     )
