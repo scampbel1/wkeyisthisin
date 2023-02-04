1914c7059423cd38f3ba27bdf96aad50076c7ca6
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-04 20:39:06 +0000

    Basic redirect is working as expected!

---
+++ Keyify.Web/Controllers/Quicklink/QuickLinkController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keyify.Web.Controllers.Quicklink
+{
+    public class QuickLinkController : Controller
+    {
+        [HttpGet("/{code}")]
+        public IActionResult Index(string code)
+        {
+            //TODO: Do not merge until this is completed
+
+            return RedirectToAction("Index", "Guitar");
+        }
+    }
+}
