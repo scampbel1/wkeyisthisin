018d9329e4807da3f42332f69f4c20ed14c1cc77
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-04 21:07:24 +0000

    Renamed variables in QuicklinkUnitTest

--- Keyify.Unit.Test/QuickLink.UnitTests/QuicklinkUnitTest.cs
+++ Keyify.Unit.Test/QuickLink.UnitTests/QuicklinkUnitTest.cs
@@ -1,13 +1,13 @@
 ﻿using Keyify.Web.Enums;
 using Keyify.Web.Enums.Tuning;
 using KeyifyClassLibrary.Enums;
-using Xunit;
-using System.Text;
 using System;
-using System.Linq;
-using System.Collections.Generic;
 using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
 using System.Text.Json;
+using Xunit;
 
 namespace Keyify.Web.Unit.Test.QuickLink.UnitTests
 {
@@ -17,33 +17,37 @@ namespace Keyify.Web.Unit.Test.QuickLink.UnitTests
         public void CreateBase64StringFromParams_DeserializeBase64String_SameParametersValuesReturned()
         {
             //Arrange - Given
-            var parameters = new { 
+            var parameters = new
+            {
                 instrumentType = InstrumentType.Guitar,
                 tuning = GuitarTuning.Standard,
                 selectedNotes = new Note[] { Note.A, Note.E, Note.Gb },
-                selectedScale = "GbAeolian" };
+                selectedScale = "GbAeolian"
+            };
 
-            var sameParametersNotesInDifferentOrder = new { 
+            var sameParametersNotesInDifferentOrder = new
+            {
                 instrumentType = InstrumentType.Guitar,
                 tuning = GuitarTuning.Standard,
                 selectedNotes = new Note[] { Note.E, Note.A, Note.Gb },
-                selectedScale = "GbAeolian" };
+                selectedScale = "GbAeolian"
+            };
 
             //Act - When
-            var parametersHashed = GetHashCodeImplementation(
+            var parametersHashCode = GetHashCodeImplementation(
                 parameters.instrumentType,
                 parameters.tuning,
                 parameters.selectedNotes,
                 parameters.selectedScale);
 
-            var sameParametersHashed = GetHashCodeImplementation(
+            var sameParametersHashCode = GetHashCodeImplementation(
                 sameParametersNotesInDifferentOrder.instrumentType,
                 sameParametersNotesInDifferentOrder.tuning,
                 sameParametersNotesInDifferentOrder.selectedNotes,
                 sameParametersNotesInDifferentOrder.selectedScale);
 
-            var parametersJson = JsonSerializer.Serialize(parametersHashed);
-            var parametersJson2 = JsonSerializer.Serialize(sameParametersHashed);
+            var parametersJson = JsonSerializer.Serialize(parametersHashCode);
+            var parametersJson2 = JsonSerializer.Serialize(sameParametersHashCode);
             var parametersJsonBytes = Encoding.Default.GetBytes(parametersJson);
             var parametersJsonBytes2 = Encoding.Default.GetBytes(parametersJson2);
             var parametersBase64String = Convert.ToBase64String(parametersJsonBytes);
@@ -55,14 +59,14 @@ namespace Keyify.Web.Unit.Test.QuickLink.UnitTests
 
         private static int GetHashCodeImplementation(InstrumentType instrumentType, GuitarTuning tuning, Note[] selectedNotes, string selectedScale)
         {
-            int hash = 17;
+            var hashCode = 17;
 
-            hash = hash * 23 + instrumentType.GetHashCode();
-            hash = hash * 23 + tuning.GetHashCode();
-            hash = hash * 23 + ((IStructuralEquatable)selectedNotes.OrderBy(n => n).ToArray()).GetHashCode(EqualityComparer<Note>.Default);
-            hash = hash * 23 + selectedScale.GetHashCode();
+            hashCode = hashCode * 23 + instrumentType.GetHashCode();
+            hashCode = hashCode * 23 + tuning.GetHashCode();
+            hashCode = hashCode * 23 + ((IStructuralEquatable)selectedNotes.OrderBy(n => n).ToArray()).GetHashCode(EqualityComparer<Note>.Default);
+            hashCode = hashCode * 23 + selectedScale.GetHashCode();
 
-            return hash;
+            return hashCode;
         }
     }
 }
