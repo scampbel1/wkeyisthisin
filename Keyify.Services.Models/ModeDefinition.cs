bf34229100004cc9da08a0ac4ec90fa287aad1fd
Sean <22592487+scampbel1@users.noreply.github.com>
2023-02-19 20:48:15 +0000

    #26 Tidied up ModeDefinition

--- Keyify.Services.Models/ModeDefinition.cs
+++ Keyify.Services.Models/ModeDefinition.cs
@@ -11,11 +11,9 @@ namespace Keyify.Services.Models
         //Create scales of all notes by default. Some scales are limited to a subset of notes.
         public readonly Array ExplicitNotesForMode = Enum.GetValues(typeof(Note));
 
-        //TODO: This breaks the Single Responsibility Principle - create something else for generating the chords
         public ModeDefinition(Mode mode, Interval[] scaleSteps, string[] scaleDegrees)
         {
             Mode = mode;
-            //ChordTypes = chordTypes;
             ScaleSteps = scaleSteps;
             ScaleDegrees = scaleDegrees;
 
@@ -28,11 +26,6 @@ namespace Keyify.Services.Models
             {
                 throw new ArgumentException($"{nameof(ScaleDegrees)} contains duplicate(s)");
             }
-
-            //if (ChordTypes != null && ChordTypes.Length != scaleSteps.Length)
-            //{
-            //    throw new ArgumentException($"{nameof(ChordTypes)} length was not equal to length of {nameof(ScaleSteps)}");
-            //}
         }
 
         public ModeDefinition(Mode mode, Interval[] scaleSteps, string[] scaleDegrees, Array explicitNotesForMode) : this(mode, scaleSteps, scaleDegrees)
