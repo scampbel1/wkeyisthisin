a9bc290e707628db0f64bd966dad9907c9cc3dd0
Sean <22592487+scampbel1@users.noreply.github.com>
2023-03-11 14:08:27 +0000

    #42 - Created Controller and Views for Music Theory and Chord Templates

---
+++ Keyify.Web/Controllers/Music Theory/ChordTemplateController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keyify.Web.Controllers.Music_Theory
+{
+    public class ChordTemplateController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Keyify.Web/Controllers/Music Theory/MusicTheoryController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Keyify.Web.Controllers.Music_Theory
+{
+    public class MusicTheoryController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
