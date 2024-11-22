fc0b2ad14ea0494103c95aef323bc3f3174da4f7
Sean Campbell <22592487+scampbel1@users.noreply.github.com>
2024-11-22 23:15:57 +0000

    Remove old code from Chord Def html service

--- Keyify.Service/ChordDefinitionsGroupingHtmlService.cs
+++ Keyify.Service/ChordDefinitionsGroupingHtmlService.cs
@@ -22,42 +22,5 @@ namespace Keyify.Web.Service
 
             return sb.ToString();
         }
-
-        private void GeneratePopulatedRows(StringBuilder sb, IEnumerable<ChordDefinition> chordDefinitions)
-        {
-            var count = 0;
-            var chordDefinitionsList = chordDefinitions.ToList();
-
-            while (count < chordDefinitions.Count())
-            {
-                sb.Append("<tr>");
-
-                if (chordDefinitions.Count() - count >= 2)
-                {
-                    AddScalesToNoteSet(chordDefinitionsList[count], sb, ref count, false);
-                    AddScalesToNoteSet(chordDefinitionsList[count], sb, ref count, true);
-                }
-                else
-                {
-                    AddScalesToNoteSet(chordDefinitionsList[count], sb, ref count, false);
-
-                    sb.Append($"<td></td>");
-                    sb.Append($"<td></td>");
-                }
-
-                sb.Append("</tr>");
-            }
-        }
-
-        private void AddScalesToNoteSet(ChordDefinition chordDefinitions, StringBuilder sb, ref int count, bool isNeighbouringScaleGroup)
-        {
-            count = isNeighbouringScaleGroup ? count += 1 : count;
-
-            sb.Append($"<td class=\"scaleResultColumn\">");
-            sb.Append($"<span class=\"scaleResult scaleText\">{chordDefinitions.Name}</span>");
-            sb.Append($"</td>");
-
-            count++;
-        }
     }
 }
