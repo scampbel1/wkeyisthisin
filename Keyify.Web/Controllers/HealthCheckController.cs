ae1ed75d17b57402e2d14b5ba920f722d5dbb2dd
Sean Campbell <22592487+scampbel1@users.noreply.github.com>
2024-11-26 22:15:49 +0000

    Show cache count in health check

--- Keyify.Web/Controllers/HealthCheckController.cs
+++ Keyify.Web/Controllers/HealthCheckController.cs
@@ -1,13 +1,17 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Keyify.Services.Models;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Data.SqlClient;
+using Microsoft.Extensions.Caching.Memory;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections.Generic;
 
 namespace Keyify.Web.Controllers
 {
-    public class HealthCheckController(IConfiguration configuration) : Controller
+    public class HealthCheckController(IConfiguration configuration, IMemoryCache memoryCache) : Controller
     {
         private readonly IConfiguration _configuration = configuration;
+        private readonly IMemoryCache _memoryCache = memoryCache;
 
         [HttpGet]
         public ActionResult Index()
@@ -21,9 +25,32 @@ namespace Keyify.Web.Controllers
                     dbStatus,
                     dbMessage,
                 },
+                cache = new
+                {
+                    chordDefinitions = GetChordDefinitionCount(),
+                    scaleDefinitions = GetScaleDefinitionCount(),
+                },
             };
 
             return Ok(info);
+
+            int GetChordDefinitionCount()
+            {
+                List<ChordDefinition> chordDefinitions;
+
+                _memoryCache.TryGetValue("ChordDefinitions", out chordDefinitions!);
+
+                return chordDefinitions.Count;
+            }
+
+            int GetScaleDefinitionCount()
+            {
+                List<ScaleDefinition> scaleDefinitions;
+
+                _memoryCache.TryGetValue("ScaleDefinitions", out scaleDefinitions!);
+
+                return scaleDefinitions.Count;
+            }
         }
 
         private (string, string) DatabaseCheck()
